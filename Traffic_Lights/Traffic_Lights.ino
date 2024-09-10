be63412e6e8a74fa42fd428ada35874300dd3539
AndreasKG <andreaskruse@hotmail.dk>
2024-09-10 10:40:59 +0200

    Final iteration of Traffic Lights project

--- Traffic_Lights/Traffic_Lights.ino
+++ Traffic_Lights/Traffic_Lights.ino
@@ -19,12 +19,12 @@ pinMode(LEDGreenA, OUTPUT);
 pinMode(LEDYellowA, OUTPUT);
 pinMode(LEDRedA, OUTPUT);
 
-test(LEDGreen);
+/*test(LEDGreen);
 test(LEDYellow);
 test(LEDRed);
 test(LEDGreenA);
 test(LEDYellowA);
-test(LEDRedA);
+test(LEDRedA);*/
   Serial.begin(9600);
   // put your setup code here, to run once:
   
@@ -33,15 +33,56 @@ test(LEDRedA);
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // Starting point
+  digitalWrite(LEDRed, HIGH);
+  digitalWrite(LEDRedA, HIGH);
+  delay(1500);
+  // Start execution on side A
+  digitalWrite(LEDYellow, HIGH);
+  digitalWrite(LEDRedA, HIGH);
+  delay(3000);
+  // Green on side A
+  digitalWrite(LEDRed, LOW);
+  digitalWrite(LEDYellow, LOW);
+  digitalWrite(LEDGreen, HIGH);
+  digitalWrite(LEDRedA, HIGH);
+  delay(5000);
+  // Green -> yellow side A
+  digitalWrite(LEDGreen, LOW);
+  digitalWrite(LEDYellow, HIGH);
+  digitalWrite(LEDRedA, HIGH);
+  delay(2000);
+  // Yellow -> Red side A
+  digitalWrite(LEDYellow, LOW);
+  digitalWrite(LEDRed, HIGH);
+  digitalWrite(LEDRedA, HIGH);
+  delay(3000);
+
+  // Start execution on side B
+  digitalWrite(LEDYellowA, HIGH);
+  digitalWrite(LEDRed, HIGH);
+  delay(3000);
+  // Red and Yellow ON on side B
+  digitalWrite(LEDRedA, LOW);
+  digitalWrite(LEDYellowA, LOW);
+  digitalWrite(LEDGreenA, HIGH);
+  digitalWrite(LEDRed, HIGH);
+  delay(10000);
+  // Green -> yellow side B
+  digitalWrite(LEDGreenA, LOW);
+  digitalWrite(LEDYellowA, HIGH);
+  digitalWrite(LEDRed, HIGH);
+  delay(2000);
+  // Yellow -> Red side B
+  digitalWrite(LEDYellowA, LOW);
+  digitalWrite(LEDRedA, HIGH);
+  digitalWrite(LEDRed, HIGH);
+  delay(5000);
 
 
-
-
-
-
-
-digitalWrite(LEDGreen,HIGH);
+/* Toby's kode
 digitalWrite(LEDRedA,HIGH);
+digitalWrite(LEDRed, LOW);
 delay(5000);
 
 digitalWrite(LEDGreen,LOW);
@@ -51,20 +92,23 @@ delay(3000);
 digitalWrite(LEDYellow,LOW);
 digitalWrite(LEDRed,HIGH);
 digitalWrite(LEDGreenA,HIGH);
+digitalWrite(LEDRedA, LOW);
 delay(10000);
 
 digitalWrite(LEDGreenA,LOW);
 digitalWrite(LEDYellowA,HIGH);
-delay(3000)
+digitalWrite(LEDRedA, HIGH);
+delay(3000);
 
 digitalWrite(LEDYellowA,LOW);
 digitalWrite(LEDRedA,LOW);
 digitalWrite(LEDGreen,HIGH);
-
+*/
 
 }
-}
+
 int test(int LED)
 {
   digitalWrite(LED, HIGH);
 }
+
